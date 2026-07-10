11ebb5195bd5c3f851a411a4436471adb33b0dcd
iffi47 <iftikharlion958@gmail.com>
2026-07-10 18:45:44 +0500

    Refactor EventsPage to use Suspense and Await for loading events, improving loading state handling

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,6 +1,7 @@
 // import { useEffect, useState } from "react";
-import { useLoaderData, data } from "react-router-dom";
+import { useLoaderData, data, Await } from "react-router-dom";
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
  // const [isLoading, setIsLoading] = useState(false);
@@ -11,27 +12,35 @@ function EventsPage() {
  //  if(isError){
  //   return <p>{message}</p>
  //  }
+
  return (
   <>
    {/* <div style={{ textAlign: 'center' }}>
         {isLoading && <p>Loading...</p>}
         {error && <p>{error}</p>}
-      </div> */}
-   <EventsList events={events} />
+      </div>export const loader =  () => {
+ 
+}; */}
+   <Suspense fallback={<p>Loading....</p>}>
+    <Await resolve={events}>
+     {(laodedEvents) => <EventsList events={laodedEvents} />}
+    </Await>
+   </Suspense>
   </>
  );
 }
 
 export default EventsPage;
-
-export const loader = async () => {
+async function loadEvents(params) {
  const response = await fetch("http://localhost:8080/events");
  if (!response.ok) {
-  // throw new Response(
-  //  JSON.stringify({ message: "Could not fetch events!" }, { status: 500 }),
-  // );
-  throw data({ message: "Count not fetch events!" }, { status: 500 });
- } else {
-  return response;
+  throw data({ message: "Could not fetch events!" }, { status: 500 });
  }
-};
+ const resData = await response.json();
+ return resData.events;
+}
+export async function loader() {
+ return {
+  events: loadEvents(),
+ };
+}
